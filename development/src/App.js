919eab65a35d685ce0fe869c0e9153ec6e15d4e1
Wadah Esam <wadah-wleed@hotmail.com>
2018-10-26 11:24:53 +0300

    redoing stuff

--- development/src/App.js
+++ development/src/App.js
@@ -1,26 +1,150 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import _ from 'lodash';
+import SelectField from './components/SelectField';
+import CurrentCalculation from './components/CurrentCalculation';
 
 class App extends Component {
+
+  state = {
+    subjects: [],
+    hours: '',
+    gpa: '',
+    points: '',
+    method: 'gpa',
+  }
+
+  componentWillMount() {
+    // adding the default empty subjects
+    let subjects = [];
+    for (let i = 0; i < 5; i++) {
+      subjects.push({
+        name: '',
+        hours: '2',
+        grade: 'A+'
+      })
+    }
+    this.setState({subjects});
+  }
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="App">
+          <img align="middle" id="logo" src="img/logo.png" alt="ksu-logo" />
+        </div>
+    
+        <div className="main-container" >
+          {/* Hours/Gpa/Points manula input */}
+          <fieldset style={{ display: 'inline'}} className="student-info" dir="rtl" >
+            <legend>الساعات / المعدل التراكمي</legend>
+            <table>
+              <tbody>
+                <tr>
+                  <td/>
+                  <td><p style={{ position: 'relative', bottom: '4px', left: '15px'}}>عدد الساعات السابقة</p></td>
+                  <td><input id="hours-text" style={{ marginBottom: '10px' }} size="3" type="number" min="1" max="200" /></td>
+                </tr>
+                <tr>
+                  <td><input checked={this.state.method==="gpa"} value="gpa" className="radio-button" type="radio" name="gpa-type" onChange={() => {this.setState({method: 'gpa'})}} /></td>
+                  <td><p>المعدل التراكمي</p></td>
+                  <td><input disabled={this.state.method!=="gpa"} id="gpa-text" type="number" min="1" max="5" /></td>
+                </tr>
+                <tr>
+                  <td><input checked={this.state.method==="points"} value="points" className="radio-button" type="radio" name="gpa-type" onChange={() => {this.setState({method: 'points'})}} /></td>
+                  <td><p>النقاط التراكمية</p></td>
+                  <td><input disabled={this.state.method!=="points"} id="points-text" type="number" min="1" max="200" /></td>
+                </tr>
+              </tbody>
+            </table>
+          </fieldset>
+
+          {/* Table of subjects */}
+          <table align="center" dir="rtl" className="subjects-table">
+            <thead>
+                <tr>
+                  <th/>
+                  <th>اسم المادة</th>
+                  <th>الساعات</th>
+                  <th>التقدير</th>
+                </tr>
+            </thead>
+            {this.renderSubjects()}
+          </table>
+          
+          {/* Results */}
+          <CurrentCalculation state={this.state} />
+
+        </div>
       </div>
+    );
+  }
+
+  renderSubjects() {
+    const renderedSubjects = this.state.subjects.map((input, index) => {
+      return(
+        <tr key={index}>
+          <td>{index+1}</td>
+          <td>
+            <input 
+            size="3" 
+            className="subject-name" 
+            value={this.state.subjects[index].name} 
+            onChange={(e) => {
+              let subjects = _.cloneDeep(this.state.subjects);
+              subjects[index].name = e.target.value;
+              this.setState({subjects: subjects});
+            }} 
+            />
+          </td>
+          <td>
+            <select 
+            defaultValue={this.state.subjects[index].hours} 
+            onChange={(e) => {
+              let subjects = _.cloneDeep(this.state.subjects);
+              subjects[index].hours = e.target.value;
+              this.setState({subjects: subjects});
+            }}
+            >
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+            </select>
+          </td>
+          <td>
+            <select 
+              defaultValue={this.state.subjects[index].grade} 
+              onChange={(e) => {
+                let subjects = _.cloneDeep(this.state.subjects);
+                subjects[index].grade = e.target.value;
+                this.setState({subjects: subjects});
+              }}
+              >
+                <option>A+</option>
+                <option>A</option>
+                <option>B+</option>
+                <option>B</option>
+                <option>C+</option>
+                <option>C</option>
+                <option>D+</option>
+                <option>D</option>
+            </select>
+          </td>
+        </tr>
+      );
+    });
+    return(
+      <tbody>
+        {renderedSubjects}
+      </tbody>
     );
   }
 }
---
+++ development/src/components/CurrentCalculation.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class CurrentCalculation extends Component {
+  render() {
+    return (
+      <fieldset style={{display: 'inline'}} dir="rtl">
+        <legend>الفصلي</legend>
+        <table>
+            <tr>
+                <td><p>النقاط</p></td>
+                <td><p id="currentPoints" >50.00</p></td>
+            </tr>
+            <tr>
+                <td><p>المعدل</p></td>
+                <td><p id="currentGPA">5.00</p></td>
+            </tr>
+            <tr>
+                <td><p>التقدير</p></td>
+                <td><p id="currentStatment">ممتاز مرتفع</p></td>
+            </tr>
+        </table>
+      </fieldset>
+    );
+  }
+}
+
+export default CurrentCalculation;
